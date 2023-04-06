6039288f27b0fd435afc9da26354a5ecbf898d18
Jidro.Z <z3069566588@163.com>
2023-04-06 14:59:52 +0800

    Correct errors and add annotations.

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/env bash
 
+# Backup the vim configuration file in the $HOME directory.
 mv -v ~/.vimrc ~/.vimrc.bak
-cp -rfpv vimrc ~/
-mv -v ~/vimrc ~/.vimrc
+# Copy the configuration files in this directory to the $HOME directory.
+cp -rfpv vim ~/.vimrc
+# Confirm whether the name is correct, if not correct file name.
+#mv -v ~/vim ~/.vimrc
+#mv -v ~/vimrc ~/.vimrc
 
+# Backup vim configuration files in the $HOME/.vim directory.
 mv -v ~/.vim/vimrc ~/.vim/vimrc.bak
-cp -rfpv .vim ~/
+# Copy the configuration files in this directory to the $HOME/.vim directory.
+#cp -rfpv .vim ~/
